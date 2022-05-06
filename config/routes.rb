5cce3148e8f5d4d5e43a971a20d9faf79935caad
Florencia Lema <flor.lema222@gmail.com>
2022-05-06 16:33:23 -0300

    js2

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root to: 'lists#index'
   resources :lists, only: [:new, :index, :create, :show] do
     resources :bookmarks, only: [:new, :create]
-    resources :reviews, only: [:create]
+    resources :reviews, only: :create
   end
-  resources :bookmarks, only: [:destroy]
+  resources :bookmarks, only: :destroy
   resources :reviews, only: [:destroy]
 end
