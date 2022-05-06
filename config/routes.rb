907416aa4aca270b73becb96ccb55a27693d7b04
Florencia Lema <flor.lema222@gmail.com>
2022-05-06 17:11:55 -0300

    asdasd

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'lists#index'
-  resources :lists, only: [:new, :index, :create, :show] do
+  resources :lists, only: [:new, :create, :show] do
     resources :bookmarks, only: [:new, :create]
     resources :reviews, only: :create
   end
